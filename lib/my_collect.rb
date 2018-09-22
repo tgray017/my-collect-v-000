87b74223e3487a66d7f9e28717e1260a483d20ae
tgray017 <tgray017@gmail.com>
2018-09-22 19:19:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    collection << yield(array[i]).split("").first
+    collection << yield(array[i])
     i += 1
   end
   collection
